4b20fa9e8c02f35034fa114c211abec7983a7a88
kanchan raju <kanchanraju2015@gmail.com>
2025-01-09 13:29:08 +0530

    first commit

---
+++ src/main/java/com/briz/springboot_githubaction_cicd/SpringbootGithubactionCicdApplication.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_githubaction_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubactionCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubactionCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briz/springboot_githubaction_cicd/SpringbootGithubactionCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_githubaction_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubactionCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
